e69ad2c9af5b8abdfb6b92c911f10014bf21cce5
Jake Thompson <wjakethompson@gmail.com>
2020-10-28 17:31:45 -0500

    add more forecasts

--- 2020-forecasts.R
+++ 2020-forecasts.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(lubridate)
 library(rpredictit)
+library(readxl)
 
 # State and abbreviations ------------------------------------------------------
 states <- tibble(state_name = state.name,
@@ -49,9 +50,9 @@ dEcon <- dEcon %>%
 
 # PredictIt --------------------------------------------------------------------
 # https://www.predictit.org/markets/13/Prez-Election
-markets <- all_markets()
+dPredit <- all_markets()
 
-dPredit <- markets %>%
+dPredit <- dPredit %>%
   filter(str_detect(name, "in the 2020 presidential") |
            str_detect(name, "in the  2020 presidential"),
          !str_detect(name, "Electoral College"),
@@ -73,17 +74,87 @@ dPredit <- markets %>%
   arrange(state_name) %>%
   mutate(model = "PredictIt", .before = 1)
 
+# Jack Kersting ----------------------------------------------------------------
+# https://projects.jhkforecasts.com/presidential-forecast/
+dJHK <- read_csv("https://data.jhkforecasts.com/2020-presidential.csv")
+
+dJHK <- dJHK %>%
+  filter(state != "US") %>%
+  select(forecastDate, state, candidate, win) %>%
+  rowid_to_column() %>%
+  group_by(state, candidate) %>%
+  slice_max(order_by = rowid, n = 1) %>%
+  ungroup() %>%
+  mutate(state = str_replace_all(state, "Maine CD", "ME"),
+         state = str_replace_all(state, "Nebraska CD", "NE"),
+         short_name = case_when(str_detect(candidate, "Trump") ~ "trump",
+                                str_detect(candidate, "Biden") ~ "biden"),
+         win = win / 100) %>%
+  filter(!is.na(short_name)) %>%
+  select(state, short_name, win) %>%
+  pivot_wider(names_from = short_name, values_from = win) %>%
+  full_join(states, by = c("state" = "state_name")) %>%
+  select(state_name = state, state_abbr, trump, biden) %>%
+  arrange(state_name) %>%
+  mutate(model = "JHK", .before = 1)
+
+# Lean Tossup ------------------------------------------------------------------
+# https://leantossup.ca/us-presidency/
+temp <- tempfile()
+download.file("http://www.leantossup.ca/US_Pres_2020/US_State_Results.xlsx", temp)
+dLT <- read_excel(temp, range = cell_cols("K:BN"))
+unlink(temp)
+
+dLT <- dLT %>%
+  pivot_longer(cols = everything(), names_to = "state_name",
+               values_to = "winner") %>%
+  count(state_name, winner) %>%
+  complete(state_name, winner, fill = list(n = 0)) %>%
+  group_by(state_name) %>%
+  mutate(pct = n / sum(n),
+         winner = tolower(winner),
+         state_name = str_replace_all(state_name, "-0", "-")) %>%
+  ungroup() %>%
+  select(-n) %>%
+  pivot_wider(names_from = winner, values_from = pct) %>%
+  full_join(states, by = "state_name") %>%
+  select(state_name, state_abbr, trump, biden) %>%
+  arrange(state_name) %>%
+  mutate(model = "Lean Tossup", .before = 1)
+
+# Princeton Election Consortium ------------------------------------------------
+# pec: https://election.princeton.edu/for-fellow-geeks/
+dPEC <- read_csv("https://election.princeton.edu/election2020/data/EV_stateprobs.csv",
+                 col_names = FALSE)
+
+dPEC <- dPEC %>%
+  select(state_abbr = X5, biden = X1) %>%
+  mutate(state_abbr = case_when(state_abbr == "M1" ~ "ME-1",
+                                state_abbr == "M2" ~ "ME-2",
+                                state_abbr == "N1" ~ "NE-1",
+                                state_abbr == "N2" ~ "NE-2",
+                                state_abbr == "N3" ~ "NE-3",
+                                TRUE ~ state_abbr),
+         biden = biden / 100,
+         trump = 1 - biden) %>%
+  full_join(states, by = "state_abbr") %>%
+  select(state_name, state_abbr, trump, biden) %>%
+  arrange(state_name) %>%
+  mutate(model = "Princeton", .before = 1)
+
+
+
+
+
+# Electoral Polls: https://electoralpolls.com/
+# Race to the White House: https://www.racetothewh.com/president
+# Reed Forecasts: https://reedforecasts.com/
+
 # Decision Desk HQ -------------------------------------------------------------
 # https://forecast.decisiondeskhq.com/president
 
+# Plural Vote ------------------------------------------------------------------
+# http://www.pluralvote.com/article/2020-forecast/
 
-
-
-
-# JHK: https://projects.jhkforecasts.com/presidential-forecast/
-# lean tossup: https://leantossup.ca/us-presidency/
-# plural vote: http://www.pluralvote.com/article/2020-forecast/
-# pec: https://election.princeton.edu/for-fellow-geeks/
-# cycle: cycle.news
-# new states men: https://www.newstatesman.com/us-election-2020
-# ourprogress: https://www.ourprogress.org/forecast
+# Progress Campaign ------------------------------------------------------------
+# https://www.ourprogress.org/forecast
