e413e6d997b0a7cfe07b40ce5a3a74f80fabe236
Jake Thompson <wjakethompson@gmail.com>
2020-11-02 16:24:17 -0600

    add election night

---
+++ election-night.R
@@ -0,0 +1,4 @@
+# election timing: https://projects.fivethirtyeight.com/election-results-timing/
+# weighted corr: https://stat.ethz.ch/R-manual/R-devel/library/stats/html/cov.wt.html
+# https://github.com/jaytimm/uspols
+# https://twitter.com/gelliottmorris/status/1323102900776783874
